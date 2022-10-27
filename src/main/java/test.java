34b683b4c6b4a58bfb4e0bc55cb2adc3462e93b4
3289006168 <106504007+3289006168@users.noreply.github.com>
2022-10-27 11:43:14 +0800

    last

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test2")
         System.out.println("test1");
     }
 }
